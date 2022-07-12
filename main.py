70544432bad0d1c826caddfc67f623e71320ab1c
allgenl <allgenl@mail.ru>
2022-07-12 10:12:18 +0300

    Add comment

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def page_index():
-    all_candidates = ""
+    """
+    Показывает список всех кандидатов
+    :return: Список всех кандидатов
+    """
     for i in range(len(candidates)):
-        if i == 0:
+        if i == 0:  # в самом начале создаётся строка all_candidates
             all_candidates = (f"Имя кандидата: {candidates[i]['name']}\n")
             all_candidates += (f"Позиция кандидата: {candidates[i]['position']}\n")
             all_candidates += (f"Навыки: {candidates[i]['skills']}\n\n")
-        elif i == len(candidates) - 1:
+        elif i == len(candidates) - 1:  # в самом конце убраны переносы строки
             all_candidates += (f"Имя кандидата: {candidates[i]['name']}\n")
             all_candidates += (f"Позиция кандидата: {candidates[i]['position']}\n")
             all_candidates += (f"Навыки: {candidates[i]['skills']}")
@@ -28,7 +31,12 @@ def page_index():
 
 @app.route('/candidate/<int:uid>')
 def page_candidate(uid):
-    if 0 < uid < len(candidates) + 1:
+    """
+    Показывает информацию о конкретном кандидате
+    :param uid: id кандидата
+    :return: Картинка, имя, позиция и навыки кандидата
+    """
+    if 0 < uid < len(candidates) + 1:  # проверяет, есть ли кандидат с таким id
         candidate = f"<img src='{candidates[uid - 1]['picture']}'>\n\n<pre>"
         candidate += (f"Имя кандидата: {candidates[uid - 1]['name']}\n")
         candidate += (f"Позиция кандидата: {candidates[uid - 1]['position']}\n")
